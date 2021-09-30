07e186d4fd4b2f242cadd3fa6af1b5849587efaa
SoraSpades <davidandrino2002@gmail.com>
2021-09-30 23:53:25 +0200

    Initial commit

---
+++ app.js
@@ -0,0 +1,52 @@
+const express = require('express')
+const app = express()
+
+const items = [
+    {
+        id: 1,
+        name: "pan",
+        price: 1.50
+    },
+    {
+        id: 2,
+        name: "cocacola",
+        price: 1
+    },
+    {
+        id: 3,
+        name: "bocadillo",
+        price: 3
+    },
+    {
+        id: 4,
+        name: "helado",
+        price: 2
+    },
+    {
+        id: 5,
+        name: "fanta",
+        price: 1
+    }
+]
+
+function searchItem(id) {
+    for (let i = 0; i < items.length; i++) 
+        if (items[i].id == id) 
+            return items[i]
+    return null
+}
+
+app.get("/query", (req, res) => {
+    if (req.query.id) {
+        const result = searchItem(parseInt(req.query.id))
+        if (result) {
+            res.send(result)
+        } else res.send({ error: "Item not Found"})
+    } else {
+        res.send({
+            error: "Send an id as query parameter"
+        })
+    }
+})
+
+module.exports = app
